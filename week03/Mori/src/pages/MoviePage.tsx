e81d8c74ef5e8f5ee1f65667b5b8dbbbdd08b5e4
sooh329 <glay@kakao.com>
2026-04-02 21:31:15 +0900

    feat: 영화 목록 페이지 UI 구현 및 tmdb 연동

---
+++ week03/Mori/src/pages/MoviePage.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { type MovieResponse, type Movie } from "../types/movies";
+import MovieCard from "../components/MovieCard";
+
+export default function MoviePage() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data } = await axios.get<MovieResponse>(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=ko-KR&page=1`
+      );
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {movies &&
+        movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+    </div>
+  );
+};
