4e66b360444b252d459d3f453e258c0faa57a61c
picnic88 <ssemilife@gmail.com>
2026-04-07 17:47:54 +0900

    chore: 미션3 파일 복구

---
+++ week03/seed03/src/theme/types/movie.ts
@@ -0,0 +1,23 @@
+export type Movie = {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    popularity: number;
+    title: string;
+    video: boolean;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    vote_average: number;
+    vote_count: number;
+};
+
+export type MovieResponse = {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+};
